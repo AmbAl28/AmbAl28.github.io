d66b0c895e91d168687627ace4078f7501e5a5da
AmbAlex <a.leha@bk.ru>
2023-12-08 22:26:29 +0500

    Развертывание на GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/ProjectGame/', // Укажите свой путь, если разворачиваетесь не в корень домена
-  plugins: [react()],
-});
-// https://vitejs.dev/config/
 // export default defineConfig({
+//   base: '/ProjectGame/', // Укажите свой путь, если разворачиваетесь не в корень домена
 //   plugins: [react()],
-// })
+// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
